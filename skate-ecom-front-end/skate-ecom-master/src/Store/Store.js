235e888de51126bf1e3d7ff79ca148e37ffa7f01
LaForge-1017 <shane.ray419@yahoo.com>
2023-09-05 15:53:09 -0500

    Added redux-persist to redux store

--- skate-ecom-front-end/skate-ecom-master/src/Store/Store.js
+++ skate-ecom-front-end/skate-ecom-master/src/Store/Store.js
@@ -1,12 +1,24 @@
-import { applyMiddleware, createStore } from "redux";
+import { applyMiddleware} from "redux";
 import reducer from "../Reducers/MainReducer";
 import thunk from "redux-thunk";
-
-const store = createStore(
-    reducer, 
-    applyMiddleware(thunk)
-    );
+import configureStore from "@reduxjs/toolkit"
+import storage from 'redux-persist/lib/storage';
+import { persistReducer, persistStore } from 'redux-persist';
 
 
+const persistConfig = {
+    key: 'root',
+    storage
+}
 
-export default store;
+const persistedReducer = persistReducer(persistConfig, reducer)
+
+
+const store = configureStore({
+    reducer: persistedReducer,
+    middleware: thunk
+});
+
+
+
+export const persistor = persistStore(store);
