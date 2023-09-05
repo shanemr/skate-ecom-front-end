6ddb389d28d3f2a4f9ce706d017d8a51d706db05
LaForge-1017 <shane.ray419@yahoo.com>
2023-09-05 16:57:30 -0500

    Added redirection for invalid user vieing profile

--- skate-ecom-front-end/skate-ecom-master/src/Components/UserProfile.js
+++ skate-ecom-front-end/skate-ecom-master/src/Components/UserProfile.js
@@ -4,16 +4,23 @@ import '../Styles/UserProfile.css'
 import { useEffect} from "react";
 import { useSelector } from "react-redux";
 import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 const UserProfile = () =>{
     const serverUrl = process.env.REACT_APP_SERVER_URL;
-    const token = useSelector((state) => state.AuthReducer.token);
-    const isLoggedIn = useSelector((state) => state.AuthReducer.token);
+    const token = useSelector((state) => state.AuthReducer.user.token);
+    const isLoggedIn = useSelector((state) => state.AuthReducer.user.isLoggedIn);
+    const navigate = useNavigate();
 
     useEffect(() =>{
-        axios.get(serverUrl + '/profile', token)
+        if(isLoggedIn){
+            axios.get(serverUrl + '/profile', {Authorization: `Bearer ${token}`})
             .then( r => console.log(r))
             .catch(e => console.log(e))
+        } else{
+            navigate('/login');
+        }
+        
     })
 
 
